d560d545eea6b56d740f544de3e81999df04d287
ayoog <20098135@mail.wit.ie>
2023-02-09 16:12:27 +0000

    Initial commit

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,31 @@
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun testOrderAndBoundaries(){
+        assertEquals(9, findLargest(intArrayOf(9, 8, 7)))
+        assertEquals(9, findLargest(intArrayOf(8, 9, 7)))
+        assertEquals(9, findLargest(intArrayOf(9, 7, 8)))
+    }
+
+    @Test
+    fun testDuplicates() {
+        assertEquals(9, findLargest(intArrayOf(9, 7, 9, 8)))
+    }
+
+    @Test
+    fun testForOnlyOneNumber(){
+        assertEquals(7, findLargest(intArrayOf(7)))
+    }
+
+    @Test
+    fun testNegativeNumbers(){
+        assertEquals(-7, findLargest(intArrayOf(-7,-8,-9)))
+    }
+
+    
+
+}
